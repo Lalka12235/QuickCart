a994612ae6a634c9af5a4ca5d2127c3a436eb0a5
Egor <fisher12235@mail.ru>
2025-06-24 14:44:05 +0700

    user service

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel, UUID4, EmailStr
 
-class User(BaseModel):
-    email: EmailStr
+class UserCreateSchema(BaseModel):
+    email: str
     username: str
+    password: str
+
+class UserOutSchema(BaseModel):
+    id: UUID4
+    email: str
+    username: str
+
+
+class UserDeleteSchema(BaseModel):
+    email: str
     password: str
---
+++ app/services/user_service.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException,status
+from app.repositories.user_repo import UserRepository
+from app.schemas.user_schema import UserCreateSchema,UserDeleteSchema
+
+class UserService:
+
+    @staticmethod
+    def create_user(user: UserCreateSchema) -> dict:
+        exist_user = UserRepository.get_user_by_email(user.email)
+
+        if exist_user:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail='User exist'
+            )
+        
+        result = UserRepository.create_user(user.email,user.username,user.password)
+
+        return {
+        "success": True,
+        "detail": "user created",
+        "result": 
+            {
+            "id": str(result.id),
+            "email": result.email,
+            "username": result.username
+            }
+        }
+    
+
+    @staticmethod
+    def delete_user(user: UserDeleteSchema) -> dict:
+        exist_user = UserRepository.get_user_by_email(user.email)
+
+        if not exist_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User dont exist'
+            )
+        
+        result = UserRepository.delete_user(user.email,user.password)
+
+        return {
+        "success": True,
+        "detail": "user delete",
+        }
